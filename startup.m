dc81ba3d7b7369e7057741d5c400b3c9533e992b
jakob <jakob.macke@gmail.com>
2014-05-15 12:01:44 +0200

    added maxent_MCMC sampling code for large Ising models and demo-script

---
+++ startup.m
@@ -0,0 +1,16 @@
+addpath([pwd,'/demo'])
+addpath([pwd,'/pop_spike'])
+addpath([pwd,'/temp_files'])
+addpath([pwd,'/maxent'])
+addpath([pwd,'/maxent_MCMC'])
+addpath([pwd,'/dich_gauss'])
+addpath([pwd,'/flat_models'])
+addpath([pwd,'/util'])
+
+%compile C_Code  if neceessary
+if 1
+    cd ./maxent_MCMC/C_Code
+    ! make all
+    cd ../..
+end
+
