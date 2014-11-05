1d64742d76bdb1af9bcd7bc706f5fe1b497dd4bd
mnonnenm <marcel.nonnenmacher@student.uni-tuebingen.de>
2014-11-06 00:24:29 +0100

    Wrote a STUB for code for iterative scaling. There's literally NO CONTENT in this file. Just serves to remind me I'll have to write this. Soon.

---
+++ maxent/iterScaling.m
@@ -0,0 +1,12 @@
+function [lambda] = iterScaling(data, fitoptions)
+% code for fitting a maxEnt model with (improved?) iterative scaling
+
+% 1. Input formatting and pre-computations
+%------------------------------------------
+
+
+% 2. Start the update iteration
+%------------------------------------------
+
+
+end
