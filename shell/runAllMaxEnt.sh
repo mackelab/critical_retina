38fa95722955b9741109db424a5776d961506258
Florian Franzen <FlorianFranzen@gmail.com>
2015-03-27 17:03:33 +0100

    Add stimulus argument to max entropy shell run script.

--- shell/runAllMaxEnt.sh
+++ shell/runAllMaxEnt.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
+if [ "$#" -ne 1 ]; then
+	echo "Usage: $0 stimulus"
+	exit 1
+fi
+STIMULUS="$1"
+
 WORKING_DIR="/gpfs01/bethge/home/ffranzen/criticality"
 MATLAB_BIN="/usr/local/MATLAB/R2014a/bin/matlab"
-STIMULUS="cb2"
 
 for NET_SIZE in $(seq 2 2 12); do
 	for IDX_DATA in $(seq 1 5); do
         qsub -S /bin/bash \
         	 -o "$WORKING_DIR/logs/" \
-        	 -j oe -l nodes=1,vmem=5G,walltime=3:0:0 \
+        	 -j oe \
+		 -l nodes=1:ppn=1,vmem=5G,walltime=73:0:0 \
         	 -N "hJV${STIMULUS}_${NET_SIZE}_${IDX_DATA}" \
         	 -d "${WORKING_DIR}/code" \
         	 -v MATLAB_BIN=${MATLAB_BIN},NET_SIZE=${NET_SIZE},IDX_DATA=${IDX_DATA},STIMULUS=${STIMULUS} \
